ba3e5a2a0e8b49f678e0f887d17fae4548cc4220
Ira S <iryna.shevchenko2@gmail.com>
2024-02-18 22:50:47 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy } from 'react';
+import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import NotFound from './components/NotFound/NotFound';
 import SharedLayout from './components/SharedLayout/SharedLayout';
